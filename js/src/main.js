c5bd88e2089d5a775efbf6fc695e7c9476532d66
Maria Vajdova <majavajdova@gmail.com>
2016-04-21 09:53:50 +0200

    pred refactoringom

--- js/src/main.js
+++ js/src/main.js
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 require('fullcalendar')
 import {} from 'qtip2'
-import {} from './druhy'
 var moment = require('moment')
 require('bootstrap')
 var DateTimeField = require('react-bootstrap-datetimepicker');
